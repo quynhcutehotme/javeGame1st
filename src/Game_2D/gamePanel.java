948df91aee282c4e124cd1312d4600dd6dfa3024
quynhcutehotme <quynhkhl17@gmail.com>
2025-09-14 20:26:57 +0700

    Nội dung thay đổi

--- src/Game_2D/gamePanel.java
+++ src/Game_2D/gamePanel.java
@@ -39,53 +39,53 @@ public class gamePanel extends JPanel implements Runnable {
 
     }
 
-//    public void run(){
-//
-//        double drawInterval = 1000000000/FPS ;
-//        double nextDrawTime = System.nanoTime() + drawInterval;
-//        while (gameThread != null){
-////            System.out.println("game running");
-//            update();
-//            repaint();
-//
-//            try {
-//                double remaniningTime = nextDrawTime - System.nanoTime();
-//
-//                if (remaniningTime < 0) {
-//                    remaniningTime = 0;
-//                }
-//                Thread.sleep((long) remaniningTime / 1000000);
-//                nextDrawTime+=drawInterval;
-//            }
-//            catch (InterruptedException e){
-//                e.printStackTrace();
-//            }
-//            }
-//
-//        }
+    public void run(){
 
-public void run() {
-    // lấy thời điểm bắt đầu
-    double drawInterval = 1000000000.0 / FPS; // thời gian 1 frame lý tưởng (ns)
-    double delta = 0;
-    long lastTime = System.nanoTime();
-    long currentTime;
-
-    while (gameThread != null) {
-        currentTime = System.nanoTime();
-
-        // cộng dồn thời gian đã trôi qua
-        delta += (currentTime - lastTime) / drawInterval;
-        lastTime = currentTime;
-
-        // nếu đã đủ thời gian cho 1 frame
-        if (delta >= 1) {
+        double drawInterval = 1000000000/FPS ;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+        while (gameThread != null){
+//            System.out.println("game running");
             update();
             repaint();
-            delta--;
+
+            try {
+                double remaniningTime = nextDrawTime - System.nanoTime();
+
+                if (remaniningTime < 0) {
+                    remaniningTime = 0;
+                }
+                Thread.sleep((long) remaniningTime / 1000000);
+                nextDrawTime+=drawInterval;
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            }
+
         }
-    }
-}
+
+//public void run() {
+//    // lấy thời điểm bắt đầu
+//    double drawInterval = 1000000000.0 / FPS; // thời gian 1 frame lý tưởng (ns)
+//    double delta = 0;
+//    long lastTime = System.nanoTime();
+//    long currentTime;
+//
+//    while (gameThread != null) {
+//        currentTime = System.nanoTime();
+//
+//        // cộng dồn thời gian đã trôi qua
+//        delta += (currentTime - lastTime) / drawInterval;
+//        lastTime = currentTime;
+//
+//        // nếu đã đủ thời gian cho 1 frame
+//        if (delta >= 1) {
+//            update();
+//            repaint();
+//            delta--;
+//        }
+//    }
+//}
 
     public void update(){
         if (keyH.upPress == true){
